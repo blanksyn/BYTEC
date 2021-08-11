eca8ce296f31089a40cea9ab9d0a2fa170697f16
oleman159 <73979786+oleman159@users.noreply.github.com>
2021-08-11 23:46:14 +0800

    Add files via upload

---
+++ src/Files/WM_genRpt_in_POView_Controller.java
@@ -0,0 +1,331 @@
+package Files;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
+
+public class WM_genRpt_in_POView_Controller extends WM implements Initializable{
+
+    @FXML
+    private Button closeBtn;
+
+    @FXML
+    private ImageView logoutBtn;
+
+    @FXML
+    private Label welcomeLabel, labelMonth, labelYear;
+
+    @FXML
+    private Button accMgtBtn;
+
+    @FXML
+    private Button WHEnvBtn;
+
+    @FXML
+    private Button POINBtn;
+
+    @FXML
+    private Button GenRptBtn;
+
+    @FXML
+    private Button invRptBtn;
+
+    @FXML
+    private Button InbRptBtn;
+
+    @FXML
+    private Button OutbRptBtn;
+
+    @FXML
+    private Button POBtn;
+
+    @FXML
+    private Button DOBtn;
+
+    @FXML
+    private TableView<POin> tbl_POinRpt;
+
+    @FXML
+    private TableColumn<POin, Integer> col_sn;
+
+    @FXML
+    private TableColumn<POin, String> col_PONum, col_supplier, col_OrdBy, col_dateOrd, col_compDate, col_status;
+
+    @FXML
+    private TableColumn<POin, POin> col_action;
+
+    @FXML
+    private TextField TF_keyword;
+
+    @FXML
+    private Button searchBtn;
+
+    @FXML
+    private ComboBox<?> CB_field;
+
+    @FXML
+    private Button genRptBtn;
+
+    String month;
+    
+    //static Date thisStartDate, thisEndDate;
+    static String thisYear, thisMonth;
+    
+    ObservableList<POin> ObserveList = FXCollections.observableArrayList();
+    
+    static void getme(String year, String month){
+        //thisStartDate = startDate;
+        //thisEndDate = endDate;
+        thisYear = year;
+        thisMonth = month;
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+        //welcome(welcomeLabel);
+        //Calendar now = Calendar.getInstance();
+        //labelYear.setText(String.valueOf(now.get(Calendar.YEAR)));
+        labelYear.setText(thisYear);
+        /*int monthNum = now.get(Calendar.MONTH) + 1;
+        switch(monthNum){
+            case 1:
+                month = "January";
+                break;
+            case 2:
+                month = "February";
+                break;
+            case 3:
+                month = "March";
+                break;
+            case 4:
+                month = "April";
+                break;
+            case 5:
+                month = "May";
+                break;
+            case 6:
+                month = "June";
+                break;
+            case 7:
+                month = "July";
+                break;
+            case 8:
+                month = "August";
+                break;
+            case 9:
+                month = "September";
+                break;
+            case 10:
+                month = "October";
+                break;
+            case 11:
+                month = "November";
+                break;
+            case 12:
+                month = "December";
+                break;
+            
+        }*/
+        labelMonth.setText(thisMonth);
+        viewPOinReportWM(thisMonth, thisYear, tbl_POinRpt, ObserveList, col_sn, col_PONum, col_supplier, col_OrdBy,
+            col_dateOrd, col_compDate, col_status, col_action);
+    }
+
+    @FXML
+    void Nav_Gen_Rpt(ActionEvent event) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("WM_genRpt_inv.fxml"));
+            Stage loginStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            loginStage.setScene(scene);
+            loginStage.centerOnScreen();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    @FXML
+    void Nav_InDO(ActionEvent event) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("WM_genRpt_in_DO.fxml"));
+            Stage loginStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            loginStage.setScene(scene);
+            loginStage.centerOnScreen();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    @FXML
+    void Nav_InPO(ActionEvent event) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("WM_genRpt_in_PO.fxml"));
+            Stage loginStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            loginStage.setScene(scene);
+            loginStage.centerOnScreen();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    @FXML
+    void Nav_InbRpt(ActionEvent event) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("WM_genRpt_in_PO.fxml"));
+            Stage loginStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            loginStage.setScene(scene);
+            loginStage.centerOnScreen();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    @FXML
+    void Nav_InvRpt(ActionEvent event) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("WM_genRpt_inv.fxml"));
+            Stage loginStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            loginStage.setScene(scene);
+            loginStage.centerOnScreen();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    @FXML
+    void Nav_OutbRpt(ActionEvent event) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("WM_genRpt_out_PO.fxml"));
+            Stage loginStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            loginStage.setScene(scene);
+            loginStage.centerOnScreen();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    @FXML
+    void Nav_PO_In(ActionEvent event) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("WM_POIN.fxml"));
+            Stage loginStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            loginStage.setScene(scene);
+            loginStage.centerOnScreen();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    @FXML
+    void Nav_WH_Env(ActionEvent event) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("WM_WHEnv_courierMgt.fxml"));
+            Stage loginStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            loginStage.setScene(scene);
+            loginStage.centerOnScreen();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    @FXML
+    void Nav_accMgt(ActionEvent event) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("WM.fxml"));
+            Stage loginStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            loginStage.setScene(scene);
+            loginStage.centerOnScreen();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    @FXML
+    void closeWindow(ActionEvent event) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("WM_genRpt_in_PO.fxml"));
+            Stage loginStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            loginStage.setScene(scene);
+            loginStage.centerOnScreen();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    @FXML
+    void logoutAcc(MouseEvent event) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You're about to logout!");
+        alert.setContentText("Confirm logout?");
+
+        if(alert.showAndWait().get()== ButtonType.OK){
+
+            Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+            Stage logoutStage = (Stage)logoutBtn.getScene().getWindow();
+            Scene scene = new Scene(root, 700, 650);
+            logoutStage.setTitle("Login");
+            logoutStage.setScene(scene);
+            Image image = new Image("image/logo192.png");
+            logoutStage.getIcons().add(image);
+            scene.setFill(Color.TRANSPARENT);
+            logoutStage.centerOnScreen();
+            logoutStage.show();
+
+        }
+    }
+
+    @FXML
+    void searchFunction(ActionEvent event) {
+
+    }
+
+}
